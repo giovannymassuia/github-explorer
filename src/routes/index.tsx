dbe885f3093bdfce51e5dd9df78544938bb881cc
Giovanny Massuia <giovannycmassuia@gmail.com>
2020-04-23 11:10:36 -0300

    minor changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,22 +4,22 @@ import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
-// const Routes: React.FC = () => (
-//   <BrowserRouter basename={process.env.PUBLIC_URL}>
-//     <Switch>
-//       <Route path="/" exact component={Dashboard} />
-//       <Route path="/respositories/:repository+" component={Repository} />
-//     </Switch>
-//   </BrowserRouter>
-// );
-
 const Routes: React.FC = () => (
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route path="/" exact component={Dashboard} />
       <Route path="/respositories/:repository+" component={Repository} />
     </Switch>
-  </HashRouter>
+  </BrowserRouter>
 );
+
+// const Routes: React.FC = () => (
+//   <HashRouter basename={process.env.PUBLIC_URL}>
+//     <Switch>
+//       <Route path="/" exact component={Dashboard} />
+//       <Route path="/respositories/:repository+" component={Repository} />
+//     </Switch>
+//   </HashRouter>
+// );
 
 export default Routes;
